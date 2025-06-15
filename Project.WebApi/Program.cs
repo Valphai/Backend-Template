67ee10b3d10643b366a75312998e2027c5c9f534
Valphai <48419034+Valphai@users.noreply.github.com>
2025-06-15 12:29:57 +0200

    Added AddHttpContextAccessor.

--- Project.WebApi/Program.cs
+++ Project.WebApi/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddMvc()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddHttpContextAccessor();
 builder.Services.RequestJwtTokenInSwagger();
 
 builder.Services.AddLogging();
